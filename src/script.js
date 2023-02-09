53b4eefb1f8fa66a609b176b93c76ed3ffcb8db5
jeangondorek <jeangondorek73@gmail.com>
2023-02-09 17:43:20 -0300

    update

---
+++ src/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    const slickOptions ={
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">PREV</button>',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">next</button>',
+    };
+    $(".slider").slick(slickOptions);
+
+    $(".form-submit").on("click", () => {
+        const email = $("#email").val();
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "username",//ideal por pelo backend
+            Password : "password",//ideal por pelo backend
+            To : 'them@website.com',
+            From : email,
+            Subject : "Pleas add to newsletter",
+            Body : `Add to newsletter and ${email}`,
+        }).then(
+          message => alert(message)
+        );
+    });
+});
